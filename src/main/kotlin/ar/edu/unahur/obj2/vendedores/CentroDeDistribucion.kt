e119bedf9d6a712e3a5f38796ec4afcd4090fe54
Alex <38331186+alebuo@users.noreply.github.com>
2021-04-13 19:08:59 -0300

    Subiendo actualización

--- src/main/kotlin/ar/edu/unahur/obj2/vendedores/CentroDeDistribucion.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/vendedores/CentroDeDistribucion.kt
@@ -2,7 +2,6 @@ package ar.edu.unahur.obj2.vendedores
 
 class CentroDeVendedores (val ubicacion: Ciudad){
     val vendedores = mutableListOf<Vendedor>()
-
     fun nuevoVendedor(vendedor: Vendedor) {
         if(vendedores.contains(vendedor)){
             error(String = "Error: El vendedor ya esta en la lista.")
@@ -10,18 +9,12 @@ class CentroDeVendedores (val ubicacion: Ciudad){
             vendedores.add(vendedor)
         }
     }
-
     fun vendedorEstrella() = vendedores.maxBy { a -> a.puntajeCertificaciones() }
-
     fun puedeCubrir(ciudad: Ciudad): Boolean {
         return vendedores.any { a -> a.puedeTrabajarEn(ciudad) }
     }
-
     fun vendedoresGenericos() = vendedores.filter { b -> b.otrasCertificaciones() >=1 }
-
-
     fun esRobusto(): Boolean {
         return vendedores.filter{ c -> c.esFirme() }.size >=3
     }
-
 }
--- src/main/kotlin/ar/edu/unahur/obj2/vendedores/Vendedor.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/vendedores/Vendedor.kt
@@ -2,58 +2,40 @@ package ar.edu.unahur.obj2.vendedores
 
 class Certificacion(val esDeProducto: Boolean, val puntaje: Int)
 
-    abstract class Vendedor {
-      // Acá es obligatorio poner el tipo de la lista, porque como está vacía no lo puede inferir.
-      // Además, a una MutableList se le pueden agregar elementos
-      val certificaciones = mutableListOf<Certificacion>()
-
-      // Definimos el método abstracto.
-      // Como no vamos a implementarlo acá, es necesario explicitar qué devuelve.
-      abstract fun puedeTrabajarEn(ciudad: Ciudad): Boolean
-
-      // En las funciones declaradas con = no es necesario explicitar el tipo
-      fun esVersatil() =
+abstract class Vendedor {
+  val certificaciones = mutableListOf<Certificacion>()
+  abstract fun puedeTrabajarEn(ciudad: Ciudad): Boolean
+  fun esVersatil() =
         certificaciones.size >= 3
           && this.certificacionesDeProducto() >= 1
           && this.otrasCertificaciones() >= 1
-
-      // Si el tipo no está declarado y la función no devuelve nada, se asume Unit (es decir, vacío)
-      fun agregarCertificacion(certificacion: Certificacion) {
+  fun agregarCertificacion(certificacion: Certificacion) {
         certificaciones.add(certificacion)
       }
-
-      fun esFirme() = this.puntajeCertificaciones() >= 30
-
-      fun certificacionesDeProducto() = certificaciones.count { it.esDeProducto }
-      fun otrasCertificaciones() = certificaciones.count { !it.esDeProducto }
-
-      fun puntajeCertificaciones() = certificaciones.sumBy { c -> c.puntaje }
-
-      abstract fun esInfluyente(): Boolean
+  fun esFirme() = this.puntajeCertificaciones() >= 30
+  fun certificacionesDeProducto() = certificaciones.count { it.esDeProducto }
+  fun otrasCertificaciones() = certificaciones.count { !it.esDeProducto }
+  fun puntajeCertificaciones() = certificaciones.sumBy { c -> c.puntaje }
+  abstract fun esInfluyente(): Boolean
 }
 
-// En los parámetros, es obligatorio poner el tipo
 class VendedorFijo(val ciudadOrigen: Ciudad) : Vendedor() {
   override fun puedeTrabajarEn(ciudad: Ciudad): Boolean {
     return ciudad == ciudadOrigen
   }
-
   override fun esInfluyente(): Boolean {
       return false
   }
 }
 
-// A este tipo de List no se le pueden agregar elementos una vez definida
 class Viajante(val provinciasHabilitadas: List<Provincia>) : Vendedor() {
   override fun puedeTrabajarEn(ciudad: Ciudad): Boolean {
     return provinciasHabilitadas.contains(ciudad.provincia)
   }
-
   override fun esInfluyente(): Boolean {
     return provinciasHabilitadas.sumBy { p -> p.poblacion } > 10000000
   }
 }
-
 class ComercioCorresponsal(val ciudadesConSucursal: List<Ciudad>) : Vendedor() {
   override fun puedeTrabajarEn(ciudad: Ciudad): Boolean {
     return ciudadesConSucursal.contains(ciudad)
